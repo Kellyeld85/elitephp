af357d366e8bd84f7600f595d13c6a58ab89e2d3
saltedm8 <glynnalexander@Glynn-iMac.home>
2017-02-10 11:21:38 +0000

    updated

---
+++ load-testing.php
@@ -0,0 +1,93 @@
+<!-- Preloader -->
+<div id="preloader">
+  <div id="status">&nbsp;</div>
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testing the grid</title>
+    <link rel="stylesheet" href="assets/styles.css">
+     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css">
+</head>
+
+<body>
+
+  <header class="mobileheader">
+
+  <!--Call your modal-->
+  <div class="mobilemenubutton">
+      <a id="menubutton" href="#animatedModal">☰</a>
+  </div>
+  </header>
+
+<?php include('includes/header.php') ?>
+
+    <div class="container">
+
+<div class="services">
+<div class="services-area align">
+<h1>Load Testing</h1>
+<p>Load testing is performed to determine the optimum strength of the ceiling material
+against the weight of the hanging object. This is a requirement in England when installing
+or changing the design of the chandelier.</p>
+<p>In addition to the weight of the chandelier itself, the weight of the chandelier chain must
+be taken into account when doing this test. It is therefore crucial that you enlist the help of
+professionals.</p>
+<p> A certificate of this test will be given to you on completion and we will advise you on when
+you might need to do another test to guarantee the safety of your chandelier.</p>
+</div>
+<div class="services-area"> <img src="img/about.png"></div>
+
+</div>
+
+<div class="services blue">
+  <div class="services-area"> <img src="img/about.png"></div>
+<div class="services-area">
+<ul>
+  <li>☑ Load safety test performed</li>
+  <li>☑ Load safety test certificate</li>
+  <li>☑ Cleaning service while load test is performed</li>
+  <li>☑ Cleaning materials and equipment</li>
+  <li>☑ A team of qualified chandelier operatives</li>
+</ul>
+</div>
+</div>
+
+<div class="clearfix"></div>
+
+<div class="services">
+
+<div class="services-clients"> <img src="img/about.png"></div>
+<div class="services-clients"> <img src="img/about.png"></div>
+<div class="services-clients"> <img src="img/about.png"></div>
+<div class="services-clients"> <img src="img/about.png"></div>
+
+</div>
+
+<div class="services">
+    <div class="services-area">
+      <p>something here Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+   <div class="services-area"> <p>something here Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+</div>
+
+<div class="clearfix"></div>
+
+        <?php include('includes/footer.php') ?>
+
+
+    </div>
+
+</body>
+<!-- jQuery Plugin -->
+    <script src="js/jquery-1.12.3.min.js"></script>
+    <script src="js/animatedModal.min.js"></script>
+
+    <script src="js/flip.min.js"></script>
+    <script src="js/customjs.js"></script>
+</html>
